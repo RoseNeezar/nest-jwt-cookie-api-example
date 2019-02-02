a3bd73b9cdb61f5f07a260807ae250342d9b5e07
Marko Zabreznik <marko@zabreznik.net>
2019-02-02 19:08:01 +0100

    added nice error for stale tokens

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,6 +1,12 @@
-import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  MiddlewareFunction,
+  BadRequestException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { TokenDto } from './dto/token.dto';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
@@ -10,16 +16,26 @@ export class AuthMiddleware implements NestMiddleware {
     return (req, res, next) => {
       if (req.cookies && req.cookies.token) {
         const tokenString = req.cookies.token;
-        if (this.jwtService.verify(tokenString)) {
-          const token = this.jwtService.decode(tokenString) as TokenDto;
 
-          delete token.exp;
-          delete token.iat;
+        try {
+          if (this.jwtService.verify(tokenString)) {
+            const token = this.jwtService.decode(tokenString) as TokenDto;
 
-          // extend the life of the cookie
-          res.cookie('token', this.jwtService.sign(token), { httpOnly: true });
+            delete token.exp;
+            delete token.iat;
 
-          req.token = token;
+            // extend the life of the cookie
+            res.cookie('token', this.jwtService.sign(token), {
+              httpOnly: true,
+            });
+
+            req.token = token;
+          }
+        } catch (e) {
+          if (e instanceof TokenExpiredError) {
+            throw new BadRequestException('Token is expired.');
+          }
+          throw new BadRequestException('Could not verify token.');
         }
       } else {
         req.token = null;
